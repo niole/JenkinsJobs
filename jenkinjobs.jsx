3b81934d2bc1345295a8687f8bdb6bbb9a2f2ab2
Niole Nelson <niolenelson@gmail.com>
2015-09-02 14:30:19 -0400

    show tags associated with each build

--- jenkinjobs.jsx
+++ jenkinjobs.jsx
@@ -1,17 +1,23 @@
 JenkinJobs = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData () {
-//    var feedData = Scrape.feed("https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/rssLatest");
     var builds = Posts.find({}).fetch();
     return {
       Builds: builds
     };
   },
+  displayBuildTags() {
+    return <ul>
+            {arguments[0].map(function(tag) {
+                return <li>{tag}</li>;
+              })}
+          </ul>;
+  },
   getJobs() {
     return this.data.Builds.map(function(data) {
         console.log('data: ');
-        return <li>{data.build.title}{data.pubDate}</li>;
-    });
+        return <li>{data.build.title}{data.pubDate}{this.displayBuildTags(data.build.tags)}</li>;
+    }.bind(this));
   },
   render() {
     return (
