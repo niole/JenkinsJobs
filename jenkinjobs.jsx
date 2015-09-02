7ae50d286904882b3e59f13ea5a3bd9b7a9a5c19
Niole Nelson <niolenelson@gmail.com>
2015-09-02 15:26:39 -0400

    create object that groups builds by title in jenkinjobs.jsx

--- jenkinjobs.jsx
+++ jenkinjobs.jsx
@@ -1,21 +1,36 @@
+//group by group title
+
 JenkinJobs = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData () {
     var builds = Posts.find({}).fetch();
+    var groupedBuilds = {};
+    builds.forEach(function(build) {
+      let titleContent = build.build.title.split(" ");
+      if (groupedBuilds[titleContent[0]]) {
+        groupedBuilds[titleContent[0]].push(build);
+      } else {
+        groupedBuilds[titleContent[0]] = [build];
+      }
+    });
+    console.log('groupedBuilds');
+    console.log(groupedBuilds);
     return {
       Builds: builds
     };
   },
   displayBuildTags() {
     return <ul>
-            {arguments[0].map(function(tag) {
-                return <li>{tag}</li>;
-              })}
+           {arguments[0].map(function(tag) {
+              return <li>{tag}</li>;
+            })}
           </ul>;
   },
   getJobs() {
     return this.data.Builds.map(function(data) {
-        console.log('data: ');
+        var titleArray = data.build.title.split(" ");
+        console.log('titleArray');
+        console.log(titleArray);
         return <li>{data.build.title}{data.pubDate}{this.displayBuildTags(data.build.tags)}</li>;
     }.bind(this));
   },
