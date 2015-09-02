1e8f8e59bfd63d19539f929e0fcd51f4ead00e97
Niole Nelson <niolenelson@gmail.com>
2015-09-02 16:47:09 -0400

    generalizes feed processing in reactiveStore.js

--- server/reactiveStore.js
+++ server/reactiveStore.js
@@ -1,14 +1,23 @@
 Meteor.startup(function() {
-  console.log(Scrape.feed("https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/rssLatest"));
+  var Feeds = [
+               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/rssLatest",
+               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop1.0,label=spark-test/rssAll"
+              ];
 
-  var prevJobs = Scrape.feed("https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/rssLatest");
-  prevJobs.items.forEach(function(build) {
-    if (Posts.find( { pubDate: build.pubDate.toString()}).count() < 1) {
-      var Build = {
-            pubDate: build.pubDate.toString(),
-            build: build
-            }
-      Posts.insert(Build);
-    }
+  Feeds.forEach(function(feed) {
+    var currFeed = Scrape.feed(feed);
+    currFeed.items.forEach(function(build) {
+
+      if (Posts.find( { pubDate: build.pubDate.toString()}).count() < 1) {
+
+        var Build = {
+              category: build.title,
+              pubDate: build.pubDate.toString(),
+              build: build
+              };
+
+        Posts.insert(Build);
+      }
+    });
   });
 });
