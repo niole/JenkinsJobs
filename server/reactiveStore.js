da95b768affc6b9038a8d670fc84501b85431e2f
Niole Nelson <niolenelson@gmail.com>
2015-09-03 18:11:26 -0400

    gets posts by specified range, groups them, sorts them lexographically

--- server/reactiveStore.js
+++ server/reactiveStore.js
@@ -1,9 +1,10 @@
 Meteor.startup(function() {
   var Feeds = [
+//              "https://amplab.cs.berkeley.edu/jenkins/view/All/rssAll"
                "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop1.0,label=spark-test/rssAll",
-               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.2,label=spark-test/rssAll",
-               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.0,label=spark-test/rssAll",
-               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.3,label=spark-test/rssAll"
+//               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.2,label=spark-test/rssAll",
+//               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.0,label=spark-test/rssAll",
+//               "https://amplab.cs.berkeley.edu/jenkins/view/Spark-QA-Test/job/Spark-Master-SBT/AMPLAB_JENKINS_BUILD_PROFILE=hadoop2.3,label=spark-test/rssAll"
               ];
 
   var scrapedFeeds = Feeds.map(function(feed) {
@@ -14,12 +15,17 @@ Meteor.startup(function() {
     F.items.forEach(function(build) {
 
       if (Posts.find( { statusTitle: build.title }).count() < 1) {
-        console.log('nothing with that title');
-
+        console.log('new POST');
+        if (Builds.find( {title: build.title.split(' ')[0]} ).count() < 1) {
+         console.log('new BUILD');
+          Builds.insert({title: build.title.split(' ')[0]} );
+        }
+        const buildConfig = Builds.findOne({title: build.title.split(' ')[0]});
         var Build = {
+              buildId: buildConfig._id,
               pubDate: build.pubDate.toString(),
               build: build,
-              title: F.title,
+              title: build.title.split(' ')[0],
               statusTitle: build.title
               };
 
