d6cdfda2029367c3b77b1c3a8a4c9bd306477a0c
Niole Nelson <niolenelson@gmail.com>
2015-09-07 01:31:24 -0400

    Shows all data in view

--- TableRow.jsx
+++ TableRow.jsx
@@ -6,45 +6,40 @@ TableRow = React.createClass({
     height: React.PropTypes.number.isRequired
   },
   componentDidMount() {
-     const svgContainer = d3.select("#"+this.props.buildId).append("svg")
-                                                      .attr("width", this.props.width)
-                                                      .attr("height", this.props.height);
-       const dayScale = d3.scale.linear()
-                          .domain([0, this.props.groupedData.length])
-                          .range([0, this.props.width]);
+    const svgContainer = d3.select("#"+this.props.buildId).append("svg")
+      .attr("width", this.props.width)
+      .attr("height", this.props.height);
+    const dayScale = d3.scale.linear()
+      .domain([0, this.props.groupedData.length])
+      .range([0, this.props.width]);
 
-       _.forEach(this.props.groupedData, (day, dayIdx) => {
-         const cellScale = d3.scale.linear()
-                                 .domain([0, day.length])
-                                 .range([dayScale(dayIdx), dayScale(dayIdx+1)-10]);
+    const cellDomains = _.flatten(
+      _.map(this.props.groupedData, (day, dayIdx) => {
+        const cellScale = d3.scale.linear()
+        .domain([0, day.length])
+        .range([dayScale(dayIdx), dayScale(dayIdx+1)-10]);
 
-          let cellDomain = day.map( (cellData, i) => {
-            console.log('this.props.groupedData.length');
-
-            console.log(this.props.groupedData.length);
-            console.log('dayIdx');
-
-            console.log(dayIdx);
-
-
-             return {xVal: cellScale(i), buildStatus: cellData.buildStatus};
-
-           });
-           const circles = svgContainer.selectAll("circle")
-                                       .data(
-                                          _.map(cellDomain, x => {
-                                           return {"cx": x.xVal, "color": x.buildStatus ? 'green': 'red'};
-                                         }))
-                                       .enter()
-                                       .append("circle");
-
-           const circleAttributes = circles
-                                  .attr("cx", function (d) { return d.cx; })
-                                  .attr("cy", function (d) { return "5px"; })
-                                  .attr("r", function (d) { return "5px"; })
-                                  .style("fill", function(d) { return d.color; });
-
-     });
+        return day.map( (cellData, i) => {
+          return {xVal: cellScale(i), buildStatus: cellData.buildStatus};
+        });
+      })
+    );
+    const circles = svgContainer.selectAll("circle")
+      .data(
+        // This stuff should be bound to component state or props
+        _.map(cellDomains, x => {
+          return {
+            "cx": x.xVal,
+            "color": x.buildStatus ? 'green': 'red'
+          };
+        })
+      )
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) { return d.cx; })
+      .attr("cy", function (d) { return "5px"; })
+      .attr("r", function (d) { return "5px"; })
+      .style("fill", function(d) { return d.color; });
   },
   render() {
     return (
